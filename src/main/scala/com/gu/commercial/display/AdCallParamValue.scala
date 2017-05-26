9ba9c3329d8e99cfb1d8f52ebb8cc104c8672934
Kelvin Chappell <kelvin.chappell@theguardian.com>
2017-05-26 15:19:36 +0100

    Refactor

--- src/main/scala/com/gu/commercial/display/AdCallParamValue.scala
+++ src/main/scala/com/gu/commercial/display/AdCallParamValue.scala
@@ -1,15 +1,17 @@
 package com.gu.commercial.display
 
-import com.gu.commercial.branding.Branding
+import com.gu.commercial.branding.{Brandable, BrandingFinder}
 import com.gu.commercial.display.Surge.bucket
 import com.gu.contentapi.client.model.v1.TagType._
-import com.gu.contentapi.client.model.v1.{Content, Tag}
+import com.gu.contentapi.client.model.v1.{Content, Tag, TagType}
 
 trait AdCallParamValue {
+  def nonEmpty: Boolean
+}
+
+object AdCallParamValue {
 
   private val ukNewsKeywordId = "uk/uk"
-
-  def nonEmpty: Boolean
 
   def targetValue(id: String): String = {
     def takeFinalSegment(s: String) = s.takeRight(s.length - s.lastIndexOf('/') - 1)
@@ -18,76 +20,67 @@ trait AdCallParamValue {
   }
 }
 
-trait SingleValue extends AdCallParamValue {
-  def raw: String
-  lazy val toCleanString = Cleaner.cleanValue(raw)
-  override lazy val nonEmpty = toCleanString.nonEmpty
+case class SingleValue(value: String) extends AdCallParamValue {
+
+  override val nonEmpty = value.nonEmpty
+
+  @deprecated("Use value instead")
+  val toCleanString = value
 }
 
-trait MultipleValues extends AdCallParamValue {
-  def raw: Set[String]
-  lazy val toCleanStrings = raw map Cleaner.cleanValue
-  override lazy val nonEmpty = toCleanStrings.nonEmpty && toCleanStrings.forall(_.nonEmpty)
+object SingleValue {
+
+  private def fromRawValue(raw: String) = SingleValue(Cleaner.cleanValue(raw))
+
+  def branding[T: Brandable](brandable: T, edition: String) =
+    fromRawValue(BrandingFinder.findBranding(edition)(brandable).map(_.brandingType.name.take(1)) getOrElse "")
+
+  def contentType(item: Content) = fromRawValue(item.`type`.name)
+
+  def edition(edition: String) = fromRawValue(edition.take(3))
+
+  def id(path: String) = fromRawValue(path.stripPrefix("/"))
+
+  def observer(item: Content) = fromRawValue(if (item.tags.exists(_.id == "publication/theobserver")) "t" else "")
+
+  def path(itemId: String) = fromRawValue(s"/${itemId.stripPrefix("/")}")
+
+  def shortUrl(item: Content) = fromRawValue(item.fields.flatMap(_.shortUrl).getOrElse(""))
+
+  def targetValue(tagId: String) = fromRawValue(AdCallParamValue.targetValue(tagId))
 }
 
-trait TagValues extends MultipleValues {
-  def item: Content
-  def condition(tag: Tag): Boolean
-  override lazy val raw: Set[String] = for (tag <- item.tags.toSet if condition(tag)) yield targetValue(tag.id)
+case class MultipleValues(values: Set[String]) extends AdCallParamValue {
+
+  override val nonEmpty = values.nonEmpty && values.forall(_.nonEmpty)
+
+  @deprecated("Use values instead")
+  val toCleanStrings = values
 }
 
-case class Authors(item: Content) extends TagValues {
-  override def condition(tag: Tag) = tag.`type` == Contributor
-}
+object MultipleValues {
 
-case class Blogs(item: Content) extends TagValues {
-  override def condition(tag: Tag) = tag.`type` == Blog
-}
+  private def fromRawValues(raw: Set[String]) = MultipleValues(raw map Cleaner.cleanValue)
 
-case class BrandingValue(branding: Option[Branding]) extends SingleValue {
-  override val raw = branding.map(_.brandingType.name.take(1)) getOrElse ""
-}
+  private def fromTags(item: Content)(p: Tag => Boolean) =
+    fromRawValues(for (tag <- item.tags.toSet if p(tag)) yield AdCallParamValue.targetValue(tag.id))
 
-case class ContentType(item: Content) extends SingleValue {
-  override val raw = item.`type`.name
-}
+  private def fromTagsOfType(item: Content, tagType: TagType) = fromTags(item)(_.`type` == tagType)
 
-case class EditionValue(edition: String) extends SingleValue {
-  override val raw = edition.take(3)
-}
+  def authors(item: Content) = fromTagsOfType(item, Contributor)
 
-case class ItemValue(path: String) extends SingleValue {
-  override val raw = path.stripPrefix("/")
-}
+  def blogs(item: Content) = fromTagsOfType(item, Blog)
 
-case class KeywordsAndPaidTopics(item: Content) extends TagValues {
-  override def condition(tag: Tag) = tag.`type` == Keyword || tag.paidContentType.contains("Topic")
-}
+  def keywordsAndPaidTopics(item: Content) = fromTags(item) { tag =>
+    tag.`type` == Keyword || tag.paidContentType.contains("Topic")
+  }
 
-case class ObserverValue(item: Content) extends SingleValue {
-  override val raw = if (item.tags.exists(_.id == "publication/theobserver")) "t" else ""
-}
+  def seriesAndPaidSeries(item: Content) = fromTags(item) { tag =>
+    tag.`type` == Series || tag.paidContentType.contains("Series")
+  }
 
-case class PathValue(itemId: String) extends SingleValue {
-  override val raw = s"/$itemId"
-}
+  def surgeBuckets(item: Content, surgeLookupService: SurgeLookupService) =
+    fromRawValues(bucket(surgeLookupService.pageViewsPerMinute(item.id)).map(_.toString).toSet)
 
-case class RawStringValue(s: String) extends SingleValue {
-  override val raw = s
-}
-
-case class SeriesAndPaidSeries(item: Content) extends TagValues {
-  override def condition(tag: Tag) = tag.`type` == Series || tag.paidContentType.contains("Series")
-}
-
-case class StringValue(s: String) extends SingleValue {
-  override val raw = targetValue(s)
-}
-
-case class SurgeBuckets(item: Content, surgeLookupService: SurgeLookupService) extends MultipleValues {
-  override val raw: Set[String] = bucket(surgeLookupService.pageViewsPerMinute(item.id)).map(_.toString).toSet
-}
-
-case class Tones(item: Content) extends TagValues {
-  override def condition(tag: Tag) = tag.`type` == Tone
+  def tones(item: Content) = fromTagsOfType(item, Tone)
 }
--- src/test/scala/com/gu/commercial/display/AdCallSpec.scala
+++ src/test/scala/com/gu/commercial/display/AdCallSpec.scala
@@ -9,7 +9,7 @@ class AdCallSpec extends FlatSpec with Matchers with OptionValues {
     def pageViewsPerMinute(pageId: String): Option[Int] = pageId match {
       case "books/2016/jan/03/murder-for-christmas-mystery-of-author-francis-duncan" => Some(232)
       case "technology/video/2017/feb/07/science-museum-robots-exhibition-backstage" => Some(98)
-      case _ => None
+      case _                                                                         => None
     }
   }
 
@@ -19,19 +19,19 @@ class AdCallSpec extends FlatSpec with Matchers with OptionValues {
     params.map {
       case (k, v) =>
         k.name -> (v match {
-          case sv: SingleValue => sv.toCleanString
-          case mv: MultipleValues => mv.toCleanStrings
+          case sv: SingleValue    => sv.value
+          case mv: MultipleValues => mv.values
         })
     }
 
   "pageLevelTargetingForContentPage" should "be correct for an article" in {
-    val item = TestModel.getContentItem("TagBrandedContent.json")
+    val item             = TestModel.getContentItem("TagBrandedContent.json")
     implicit val edition = "uk"
-    val params = stringify(adCall.pageLevelTargetingForContentPage(item))
+    val params           = stringify(adCall.pageLevelTargetingForContentPage(item))
     params shouldBe Map(
-      "br" -> "s",
-      "co" -> Set("dom-phillips"),
-      "ct" -> "article",
+      "br"      -> "s",
+      "co"      -> Set("dom-phillips"),
+      "ct"      -> "article",
       "edition" -> "uk",
       "k" -> Set(
         "sustainable-business",
@@ -44,7 +44,7 @@ class AdCallSpec extends FlatSpec with Matchers with OptionValues {
         "environment",
         "northernireland"
       ),
-      "p" -> "ng",
+      "p"  -> "ng",
       "se" -> Set("spotlight-on-commodities"),
       "su" -> Set("0"),
       "tn" -> Set("news"),
@@ -54,120 +54,120 @@ class AdCallSpec extends FlatSpec with Matchers with OptionValues {
   }
 
   it should "be correct for a video page" in {
-    val item = TestModel.getContentItem("VideoContent.json")
+    val item             = TestModel.getContentItem("VideoContent.json")
     implicit val edition = "uk"
-    val params = stringify(adCall.pageLevelTargetingForContentPage(item))
+    val params           = stringify(adCall.pageLevelTargetingForContentPage(item))
     params shouldBe Map(
-      "co" -> Set("jess-gormley", "alex-healey", "oliver-wainwright"),
-      "ct" -> "video",
+      "co"      -> Set("jess-gormley", "alex-healey", "oliver-wainwright"),
+      "ct"      -> "video",
       "edition" -> "uk",
-      "k" -> Set("robots", "museums", "culture", "technology", "science", "london", "uk/uk"),
-      "p" -> "ng",
-      "su" -> Set("5"),
-      "tn" -> Set("news"),
-      "url" -> "/technology/video/2017/feb/07/science-museum-robots-exhibition-backstage"
+      "k"       -> Set("robots", "museums", "culture", "technology", "science", "london", "uk/uk"),
+      "p"       -> "ng",
+      "su"      -> Set("5"),
+      "tn"      -> Set("news"),
+      "url"     -> "/technology/video/2017/feb/07/science-museum-robots-exhibition-backstage"
     )
   }
 
   it should "be correct for an Observer page" in {
-    val item = TestModel.getContentItem("ObserverContent.json")
+    val item             = TestModel.getContentItem("ObserverContent.json")
     implicit val edition = "uk"
-    val params = stringify(adCall.pageLevelTargetingForContentPage(item))
+    val params           = stringify(adCall.pageLevelTargetingForContentPage(item))
     params shouldBe Map(
-      "co" -> Set("vanessathorpe"),
-      "ct" -> "article",
+      "co"      -> Set("vanessathorpe"),
+      "ct"      -> "article",
       "edition" -> "uk",
-      "k" -> Set("crime", "books", "culture", "fiction", "uk/uk"),
-      "ob" -> "t",
-      "p" -> "ng",
-      "su" -> Set("3", "4", "5"),
-      "tn" -> Set("news"),
-      "url" -> "/books/2016/jan/03/murder-for-christmas-mystery-of-author-francis-duncan"
+      "k"       -> Set("crime", "books", "culture", "fiction", "uk/uk"),
+      "ob"      -> "t",
+      "p"       -> "ng",
+      "su"      -> Set("3", "4", "5"),
+      "tn"      -> Set("news"),
+      "url"     -> "/books/2016/jan/03/murder-for-christmas-mystery-of-author-francis-duncan"
     )
   }
 
   "pageLevelTargetingForSectionFront" should "be correct for a facia front" in {
     val section = TestModel.getSection("FoundationSection.json")
-    val params = stringify(adCall.pageLevelTargetingForSectionFront("uk")(section))
+    val params  = stringify(adCall.pageLevelTargetingForSectionFront("uk")(section))
     params shouldBe Map(
-      "br" -> "f",
-      "ct" -> "section",
+      "br"      -> "f",
+      "ct"      -> "section",
       "edition" -> "uk",
-      "k" -> "cities",
-      "p" -> "ng",
-      "url" -> "/cities"
+      "k"       -> "cities",
+      "p"       -> "ng",
+      "url"     -> "/cities"
     )
   }
 
   it should "be correct for an editionalised facia front" in {
     val section = TestModel.getSection("EditionalisedSection.json")
-    val params = stringify(adCall.pageLevelTargetingForSectionFront("uk")(section))
+    val params  = stringify(adCall.pageLevelTargetingForSectionFront("uk")(section))
     params shouldBe Map(
-      "ct" -> "section",
+      "ct"      -> "section",
       "edition" -> "uk",
-      "k" -> "culture",
-      "p" -> "ng",
-      "url" -> "/uk/culture"
+      "k"       -> "culture",
+      "p"       -> "ng",
+      "url"     -> "/uk/culture"
     )
   }
 
   "pageLevelTargetingForTagPage" should "be correct for a facia front" in {
-    val tag = TestModel.getTag("FoundationTag.json")
+    val tag    = TestModel.getTag("FoundationTag.json")
     val params = stringify(adCall.pageLevelTargetingForTagPage("uk")(tag))
     params shouldBe Map(
-      "br" -> "s",
-      "ct" -> "tag",
+      "br"      -> "s",
+      "ct"      -> "tag",
       "edition" -> "uk",
-      "p" -> "ng",
-      "se" -> "spotlight-on-commodities",
-      "url" -> "/sustainable-business/series/spotlight-on-commodities"
+      "p"       -> "ng",
+      "se"      -> "spotlight-on-commodities",
+      "url"     -> "/sustainable-business/series/spotlight-on-commodities"
     )
   }
 
   it should "be correct for a series tag page" in {
-    val tag = TestModel.getTag("SeriesTag.json")
+    val tag              = TestModel.getTag("SeriesTag.json")
     implicit val edition = "uk"
-    val params = stringify(adCall.pageLevelTargetingForTagPage("uk")(tag))
+    val params           = stringify(adCall.pageLevelTargetingForTagPage("uk")(tag))
     params shouldBe Map(
-      "ct" -> "tag",
+      "ct"      -> "tag",
       "edition" -> "uk",
-      "p" -> "ng",
-      "se" -> "new-view-series",
-      "url" -> "/film/series/new-view-series"
+      "p"       -> "ng",
+      "se"      -> "new-view-series",
+      "url"     -> "/film/series/new-view-series"
     )
   }
 
   it should "be correct for a keyword tag page" in {
-    val tag = TestModel.getTag("KeywordTag.json")
+    val tag    = TestModel.getTag("KeywordTag.json")
     val params = stringify(adCall.pageLevelTargetingForTagPage("us")(tag))
     params shouldBe Map(
-      "ct" -> "tag",
+      "ct"      -> "tag",
       "edition" -> "us",
-      "k" -> "turkey",
-      "p" -> "ng",
-      "url" -> "/lifeandstyle/turkey"
+      "k"       -> "turkey",
+      "p"       -> "ng",
+      "url"     -> "/lifeandstyle/turkey"
     )
   }
 
   "pageLevelTargetingForNetworkFront" should "be correct for a network front" in {
     val params = stringify(adCall.pageLevelTargetingForNetworkFront("au")(networkFrontPath = "/us"))
     params shouldBe Map(
-      "ct" -> "network-front",
+      "ct"      -> "network-front",
       "edition" -> "au",
-      "k" -> "us",
-      "p" -> "ng",
-      "url" -> "/us"
+      "k"       -> "us",
+      "p"       -> "ng",
+      "url"     -> "/us"
     )
   }
 
   it should "be correct for International network front" in {
     val params = stringify(adCall.pageLevelTargetingForNetworkFront("international")(networkFrontPath = "/us"))
     params shouldBe Map(
-      "ct" -> "network-front",
+      "ct"      -> "network-front",
       "edition" -> "int",
-      "k" -> "us",
-      "p" -> "ng",
-      "url" -> "/us"
+      "k"       -> "us",
+      "p"       -> "ng",
+      "url"     -> "/us"
     )
   }
 
@@ -175,11 +175,11 @@ class AdCallSpec extends FlatSpec with Matchers with OptionValues {
     val params =
       stringify(adCall.pageLevelTargetingForFrontUnknownToCapi("international")(frontId = "us/tv-and-radio"))
     params shouldBe Map(
-      "ct" -> "section",
+      "ct"      -> "section",
       "edition" -> "int",
-      "k" -> "tv-and-radio",
-      "p" -> "ng",
-      "url" -> "/us/tv-and-radio"
+      "k"       -> "tv-and-radio",
+      "p"       -> "ng",
+      "url"     -> "/us/tv-and-radio"
     )
   }
 }
